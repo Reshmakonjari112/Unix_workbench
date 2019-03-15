6dfcf5c1fffff34bb99d71b99ffa15b4f38b0381
reshmakonjari112 <reshmakonjari112@gmail.com>
2019-03-15 15:18:53 +0530

    add count directory files syntax

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #Program to count number of files present in the current directory
+count=$(ls -1 | wc -l)
 echo "Please guess the number of files are present in the current directory: "
 read num
 function game {
 	while true; do
-		if [ $num -gt 9 ]; then
+		if [ $num -gt $count ]; then
 			echo "Your guess is high"
 			echo "Please try again: "
 			read num
-		elif [ $num -lt 9 ]; then
+		elif [ $num -lt $count ]; then
 			echo "Your guess is low"
 			echo "Please try again"
 			read num
